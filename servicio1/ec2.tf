9ef05877ed7714a2526003ff7d7da2e1b4b7827b
Martin Hierro <jmhierro@gmail.com>
2021-03-29 22:31:51 -0300

    fix ec2

--- servicio1/ec2.tf
+++ servicio1/ec2.tf
@@ -5,12 +5,11 @@ provider "aws" {
 }
 
 resource "aws_instance" "srv" {
-  ami               = "ami-042e8287309f5df03"
+  ami               = "ami-0be2609ba883822ec"
   key_name          = "naas-support"
   vpc_security_group_ids = ["sg-046a78bd04f2057b2"]
   associate_public_ip_address = true
   source_dest_check  = false
-  ebs_optimized      = true
   instance_type      = "t2.micro"
   subnet_id          = "subnet-009c40c86e0ad16ff"
   tags = {
